a0ad2a6a20f3982d2ac93c511a6cfce84568c4c8
sahadan <sahadan@gmail.com>
2021-03-05 16:04:27 +0530

    First Commit

---
+++ DemoGitApp/src/com/faith/app/DemoApp.java
@@ -0,0 +1,13 @@
+package com.faith.app;
+
+public class DemoApp {
+
+	public static void main(String[] args) {
+		
+		// Demonstration on GitHub - Push / Fetch
+		System.out.println("Welcome to the World of Git");
+		
+		
+	}
+
+}
